39ace90eaebe9b0a2cbe3b80f87145c5ee1b2d32
지혜인(Hyein Ji)/Media Integration팀/SKP <hyein.ji@sk.com>
2021-06-03 13:27:03 +0900

    4.18 Ejecting from Apollo Server

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 require("dotenv").config();
-import { ApolloServer } from "apollo-server";
+import express from "express";
+import logger from "morgan";
+import { ApolloServer } from "apollo-server-express";
 import { typeDefs, resolvers } from "./schema";
 import { getUser } from "./users/users.utils";
 
+const PORT = process.env.PORT;
 const server = new ApolloServer({
-    typeDefs,
     resolvers,
+    typeDefs,
     context: async ({ req }) => {
         return {
             loggedInUser: await getUser(req.headers.token)
@@ -13,6 +16,9 @@ const server = new ApolloServer({
     }
 });
 
-const PORT = process.env.PORT;
-
-server.listen(PORT).then(() => console.log(`Server is running on http://localhost:${PORT}`));
+const app = express();
+app.use(logger("tiny"));
+server.applyMiddleware({ app });
+app.listen({ port: PORT }, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
