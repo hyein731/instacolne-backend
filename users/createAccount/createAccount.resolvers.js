bbc07e18ea2ce7497cec7d9cf1afe2e47215be2a
지혜인(Hyein Ji)/Media Integration팀/SKP <hyein.ji@sk.com>
2021-06-03 14:38:56 +0900

    4.22 Follow & Unfollow

--- users/createAccount/createAccount.resolvers.js
+++ users/createAccount/createAccount.resolvers.js
@@ -14,13 +14,19 @@ export default {
                     throw new Error("This username or password is already taken.");
                 }
                 const uglyPassword = await bcrypt.hash(password, 10);
-                return client.user.create({ 
+                await client.user.create({ 
                     data: {
                         username, email, firstName, lastName, password: uglyPassword
                     }
                 });
+                return {
+                    ok: true,
+                };
             } catch (e) {
-                return e;
+                return {
+                    ok: false,
+                    error: "Can't create account.",
+                };
             }
         },
     }
--- users/seeProfile/seeProfile.resolvers.js
+++ users/seeProfile/seeProfile.resolvers.js
@@ -3,7 +3,11 @@ import client from "../../client";
 export default {
     Query: {
         seeProfile: (_, { username }) => client.user.findUnique({
-            where: { username }
+            where: { username },
+            include: { 
+                following: true,
+                followers: true,
+            }
         }),
     },
 };
