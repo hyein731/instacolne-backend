45c173d9efcdf9e62791363d6c0205bb4934fbbb
지혜인(Hyein Ji)/Media Integration팀/SKP <hyein.ji@sk.com>
2021-06-08 15:50:03 +0900

    7.12 Recap - Backend DONE!

--- messages/roomUpdates/roomUpdates.resolvers.js
+++ messages/roomUpdates/roomUpdates.resolvers.js
@@ -1,10 +1,68 @@
 import pubsub from "../../pubsub";
 import { NEW_MESSAGE } from "../../constants";
+import { withFilter } from "apollo-server";
+import client from "../../client";
 
 export default {
     Subscription: {
         roomUpdates: {
-            subscribe: () => pubsub.asyncIterator(NEW_MESSAGE),
+            // #1. Public으로 아무나 리스닝해도 될 경우 
+            // subscribe: () => pubsub.asyncIterator(NEW_MESSAGE),
+
+            // #2. Filter 필요한 경우
+            // subscribe: withFilter(
+            //     () => pubsub.asyncIterator(NEW_MESSAGE),
+            //     ({ roomUpdates }, { id }) => {
+            //         return roomUpdates.roomId === id;
+            //     }
+            // ),
+
+            subscribe: async(root, args, context, info) => {
+                const room = await client.room.findFirst({
+                    where: {
+                        id: args.id,
+                        users: {
+                            some: {
+                                id: context.loggedInUser.id,
+                            },
+                        },
+                    },
+                    select: {
+                        id: true,
+                    },
+                });
+                if (!room) throw new Error("You shall not see this.");
+                return withFilter( // 두 파람 필요. 첫번째는 Iterator, 두번째는 어떤 상황일 때 보여줄지 (반드시 boolean 리턴)
+                    () => pubsub.asyncIterator(NEW_MESSAGE),
+                    // ({ roomUpdates }, { id }) => {
+                    //     return roomUpdates.roomId === id;
+                    // }
+
+                    // 아래처럼 굳이 할 필요는 없지만 하면 User가 리스닝 후 방에서 쫓겨났을 때 그 이후 메시지 볼 수 없음
+                    async ({ roomUpdates }, { id }, { loggedInUser }) => {
+                        if (roomUpdates.roomId === id) {
+                          const room = await client.room.findFirst({
+                            where: {
+                              id,
+                              users: {
+                                some: {
+                                  id: loggedInUser.id,
+                                },
+                              },
+                            },
+                            select: {
+                              id: true,
+                            },
+                          });
+                          if (!room) {
+                            return false;
+                          }
+                          return true;
+                        }
+                    }
+                    //
+                )(root, args, context, info);
+            },
         }
     }
 };
--- server.js
+++ server.js
@@ -11,13 +11,30 @@ const PORT = process.env.PORT;
 const apollo = new ApolloServer({
     resolvers,
     typeDefs,
-    context: async ({ req }) => {
-        if (req) {
+    context: async (ctx) => {
+        if (ctx.req) { // http
             return {
-                loggedInUser: await getUser(req.headers.token)
+                loggedInUser: await getUser(ctx.req.headers.token)
             };
+        } else { // ws
+            const { connection : { context } } = ctx;
+            return {
+                loggedInUser: context.loggedInUser
+            }
         }
-    }
+    },
+    subscriptions: {
+        onConnect: async ({ token }) => {
+            if (!token) {
+                throw new Error("You can't listen.");
+            }
+            const loggedInUser = await getUser(token);
+            return {
+                loggedInUser,
+            };
+            
+        }
+    },
 });
 
 const app = express();
